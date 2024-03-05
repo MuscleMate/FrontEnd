8461880c3ffcfb4288b6ed850ae7b8343e49d9d8
Kamil Magdziarz <kmkmagdziarz@gmail.com>
2024-03-05 18:10:06 +0100

    Login screen basic func + layout

--- app/src/main/java/com/example/musclematefront/MainActivity.java
+++ app/src/main/java/com/example/musclematefront/MainActivity.java
@@ -1,24 +1,53 @@
 package com.example.musclematefront;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.example.musclematefront.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        setupButtons();
+        //hide password confirm
+        binding.editTextPassword2.setVisibility(View.GONE);
+    }
+    private void setupButtons(){
+        binding.logInChangeButton.setOnClickListener(view -> {
+            Toast.makeText(MainActivity.this, "LoginChange", Toast.LENGTH_SHORT).show();
+            binding.editTextPassword.setVisibility(View.VISIBLE);
         });
+
+        binding.signUpChangeButton.setOnClickListener(view -> {
+            Toast.makeText(MainActivity.this, "SignupChange", Toast.LENGTH_SHORT).show();
+            binding.editTextPassword.setVisibility(View.GONE);
+            binding.editTextPassword2.setVisibility(View.GONE);
+        });
+
+        binding.logInButton.setOnClickListener(view -> {
+            Toast.makeText(MainActivity.this, "Login", Toast.LENGTH_SHORT).show();
+        });
+
+        binding.forgotPassword.setOnClickListener(view -> {
+            Toast.makeText(MainActivity.this, "forgotPassword", Toast.LENGTH_SHORT).show();
+        });
+
+        binding.facebookIcon.setOnClickListener(view -> {
+            Toast.makeText(MainActivity.this, "facebookIcon", Toast.LENGTH_SHORT).show();
+        });
+
+        binding.googleIcon.setOnClickListener(view -> {
+            Toast.makeText(MainActivity.this, "googleIcon", Toast.LENGTH_SHORT).show();
+        });
+        binding.appleIcon.setOnClickListener(view -> {
+            Toast.makeText(MainActivity.this, "appleIcon", Toast.LENGTH_SHORT).show();
+        });
+
     }
 }
