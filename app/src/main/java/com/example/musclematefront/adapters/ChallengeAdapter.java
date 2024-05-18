79bfe1571de4226f089db6a17f177b927da69fc6
Kamil Magdziarz <kmkmagdziarz@gmail.com>
2024-05-18 14:14:41 +0200

    challenge cards + activity

---
+++ app/src/main/java/com/example/musclematefront/adapters/ChallengeAdapter.java
@@ -0,0 +1,71 @@
+package com.example.musclematefront.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.musclematefront.R;
+import com.example.musclematefront.activities.ChallengeActivity;
+import com.example.musclematefront.models.Challenge;
+
+import java.util.List;
+
+public class ChallengeAdapter extends RecyclerView.Adapter<ChallengeAdapter.ViewHolder>{
+    private List<Challenge> challengesList;
+    private Context context;
+    public ChallengeAdapter(List<Challenge> challengesList){
+        this.challengesList = challengesList;
+
+
+    }
+    public void setChallengesList(List<Challenge> challengesList) {
+        this.challengesList = challengesList;
+    }
+    @Override
+    public ChallengeAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.challenge_card, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChallengeAdapter.ViewHolder holder, int position) {
+        CardView cardView = holder.cardView;
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, ChallengeActivity.class);
+                context.startActivity(intent);
+            }
+        });
+        TextView hoursLeftTextView = cardView.findViewById(R.id.hoursLeftTextView);
+        TextView levelTextView = cardView.findViewById(R.id.xpTextView);
+        TextView participantsTextView = cardView.findViewById(R.id.participantsTextView);
+        TextView workoutTitleTextView = cardView.findViewById(R.id.workoutTitleTextView);
+        TextView byTextView = cardView.findViewById(R.id.byTextView);
+        hoursLeftTextView.setText(String.valueOf(challengesList.get(position).getDuration()));
+        levelTextView.setText(challengesList.get(position).getDifficulty());
+        workoutTitleTextView.setText(challengesList.get(position).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return challengesList.size();
+    }
+
+    protected static class ViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
+
+        public ViewHolder(View v){
+            super(v);
+            cardView = (CardView) v;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/musclematefront/models/Challenge.java
@@ -0,0 +1,138 @@
+package com.example.musclematefront.models;
+
+import java.util.Date;
+
+public class Challenge {
+    private String id;
+    private String title;
+    private String description;
+    private int duration;
+    private String goal;
+    private int target;
+    private String difficulty;
+    private String status;
+    private Date startDate;
+    private Date endDate;
+    private int version;
+
+    public Challenge(String id, String title, String description, int duration, String goal, int target, String difficulty, String status, Date startDate, Date endDate, int version) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.duration = duration;
+        this.goal = goal;
+        this.target = target;
+        this.difficulty = difficulty;
+        this.status = status;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.version = version;
+    }
+
+    // Getters and setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getGoal() {
+        return goal;
+    }
+
+    public void setGoal(String goal) {
+        this.goal = goal;
+    }
+
+    public int getTarget() {
+        return target;
+    }
+
+    public void setTarget(int target) {
+        this.target = target;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    @Override
+    public String toString() {
+        return "Challenge{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", duration=" + duration +
+                ", goal='" + goal + '\'' +
+                ", target=" + target +
+                ", difficulty='" + difficulty + '\'' +
+                ", status='" + status + '\'' +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", version=" + version +
+                '}';
+    }
+}
